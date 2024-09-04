4969174fb2b9b4a2eb7ff67091cc6b283f748598
Rugved P Pund <rugvedpund@bmxdaq2.phy.bnl.gov>
2024-09-04 13:10:11 -0400

    feat: add mock data generation functions - Added functions to generate mock data for ulsa, da, and cmb. - Implemented `gaussian_noise` for generating Gaussian noise. - Implemented `da_template` for generating Dark Ages template. - Implemented `cmb_template` for generating CMB template. - Implemented `fg_template` for generating frequency domain power law.

--- simflows/load.py
+++ simflows/load.py
@@ -2,6 +2,9 @@
 
 import os
 
+import jax.numpy as jnp
+import lusee
+import numpy as np
 import xarray as xr
 
 import simflows.utils as simutils
@@ -9,15 +12,57 @@ import simflows.utils as simutils
 path_here = os.path.dirname(os.path.abspath(__file__))
 data_path = os.path.join(path_here, "../")
 
-# NOTE: argparser needs something like: python simtensors.py --config configs/a{0..80..10}_dt3600.yaml
-#
-# Load the lusee ulsa sim tensor
-print("Loading ULSA sim tensor..")
-parser = simutils.create_parser()
-args = parser.parse_args()
-sim = simutils.SimTensor().from_args(args)
-print("  saving as netcdf..")
-sim.to_netcdf("data/netcdf/sim.nc")
+
+##--------------------------------------------------------------------##
+# %%
+
+"""
+1. need mock data for ulsa, da, cmb
+    - ulsa: power law freqs x gaussian noise times
+    - da,cmb: template freqs x no noise times
+2. create named tensor class?
+"""
+
+
+def gaussian_noise(times: int = 650, sigma: float = 1e-3) -> jnp.ndarray:
+    """Gaussian noise"""
+    key = jax.random.PRNGKey(42)
+    return sigma * jax.random.normal(key, (times,))
+
+
+def da_template(freqs: jnp.ndarray = jnp.linspace(1, 50)) -> jnp.ndarray:
+    """lusee T_DarkAges_Scaled template"""
+    t21 = lusee.monosky.T_DarkAges_Scaled(freqs)
+    return jnp.array(t21)
+
+
+def cmb_template(
+    freqs: jnp.ndarray = jnp.linspace(1, 50), T_cmb: jnp.float32 = 2.75
+) -> jnp.ndarray:
+    """constant CMB template"""
+    return T_cmb * jnp.ones_like(freqs)
+
+
+def fg_template(
+    freqs: jnp.ndarray = jnp.linspace(1, 50),
+    amp30: jnp.float64 = 1e5,
+    idx: jnp.float64 = 2.54,
+) -> jnp.ndarray:
+    """Power law function for the frequency domain"""
+    return amp30 * (1 / (freqs / 30) ** idx)
+
+
+# %%
+
+# # NOTE: argparser needs something like: python simtensors.py --config configs/a{0..80..10}_dt3600.yaml
+# #
+# # Load the lusee ulsa sim tensor
+# print("Loading ULSA sim tensor..")
+# parser = simutils.create_parser()
+# args = parser.parse_args()
+# sim = simutils.SimTensor().from_args(args)
+# print("  saving as netcdf..")
+# sim.to_netcdf("data/netcdf/sim.nc")
 
 # # WARN: when loading netcdf tensor, ensure string coordniates are loaded as strings
 #
