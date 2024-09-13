cc5007ff366c4f64d7cb4f8e7f99e23468a07974
rugved@bmx <rugvedpund@gmail.com>
2024-09-12 23:57:48 -0400

    update

--- scripts/24_pcatemplates.py
+++ scripts/24_pcatemplates.py
@@ -1,25 +1,108 @@
 # %%
 
-import jax
-import jax.numpy as jnp
+from simflows.imports import *
 
-# WARN: stupid jax doesn't like float64 by default
-jax.config.update("jax_enable_x64", True)
-
-import lusee
-import matplotlib.pyplot as plt
-import numpy as np
-import xarray as xr
-import xarray_einstats
-
-import simflows.load as simloader
-import simflows.pca as simpca
-import simflows.utils as simutils
-
-##--------------------------------------------------------------------##
 # %%
-# load templates for ulsa, da, cmb
 
+ntimes = 650
+idxs = simjax.random_normal((ntimes, 1), seed=0, mean=2.5, sigma=0.5)
+amp20MHz = simjax.random_normal((ntimes, 1), seed=1, mean=1e4, sigma=1e4)
+amp20MHz = jnp.abs(amp20MHz)
+mock = simloader.load_mock_sim(idxs=idxs, amp20MHz=amp20MHz, da_amp=1)
+fg, da, cmb, sum, delta = mock
+
+# %%
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+ax[0].scatter(idxs, amp20MHz)
+ax[0].set_xlabel("spectral index")
+ax[0].set_ylabel("amplitude at 20MHz")
+fg.plot(
+    x="freqs",
+    hue="times",
+    add_legend=False,
+    yscale="log",
+    color="grey",
+    alpha=0.1,
+    ax=ax[1],
+)
+ax[1].errorbar(fg.freqs, fg.mean("times"), fg.std("times"), fmt="o-", color="C0")
+ax[1].set_yscale("symlog")
+plt.show()
+
+# %%
+
+
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+ax[0].scatter(idxs, amp20MHz)
+ax[0].set_xlabel("spectral index")
+ax[0].set_ylabel("amplitude at 20MHz")
+delta.plot(
+    x="freqs",
+    hue="times",
+    add_legend=False,
+    yscale="log",
+    color="grey",
+    alpha=0.1,
+    ax=ax[1],
+)
+ax[1].errorbar(
+    delta.freqs, delta.mean("times"), delta.std("times"), fmt="o-", color="C0"
+)
+ax[1].set_yscale("symlog")
+plt.show()
+
+
+# %%
+
+
+# %%
+
+# g=mock.sel(kind=['fg','delta']).mean('times').plot(col='kind',x="freqs", yscale='log')
+# plt.show()
+
+# %%
+
+plt.figure(figsize=(13, 3))
+plt.subplot(131)
+plt.plot(idxs)
+plt.title("spectral index")
+plt.subplot(132)
+plt.semilogy(amp20MHz)
+plt.title("amplitude")
+plt.subplot(133)
+fg.plot(norm=simutils.lognorm(), x="times")
+plt.tight_layout()
+plt.show()
+
+
+# %%
+
+mock.sel(kind=["fg", "sum", "delta"]).plot(
+    col="kind", x="times", norm=simutils.symlognorm()
+)
+plt.show()
+
+g = mock.sel(kind=["fg", "sum"]).mean("times").plot(col="kind", x="freqs", yscale="log")
+plt.show()
+
+g = (
+    mock.sel(kind=["fg", "delta"])
+    .mean("times")
+    .plot(col="kind", x="freqs", yscale="symlog")
+)
+plt.show()
+
+plt.subplot(121)
+fg.plot(norm=simutils.lognorm(), x="times")
+plt.subplot(122)
+mock["delta"].plot(x="times", norm=simutils.symlognorm())
+plt.tight_layout()
+plt.show()
+
+
+# %%
+# %%
 # templates = loader.load_templates(freqs=jnp.linspace(1, 50, 393), da_amp=4e6)
 templates = simloader.load_templates(da_amp=1)
 mock = simloader.load_mock_sim(da_amp=1)
@@ -117,7 +200,7 @@ plt.show()
 # - WARN: important note 2.
 # - BUG: important note 3.
 
-# ---
+# %%
 
 from simflows.imports import *
 
@@ -146,12 +229,12 @@ ax[1].set_yscale("symlog")
 plt.show()
 
 
-# ---
+# %%
 
 # g=mock.sel(kind=['fg','delta']).mean('times').plot(col='kind',x="freqs", yscale='log')
 # plt.show()
 
-# ---
+# %%
 
 plt.figure(figsize=(13, 3))
 plt.subplot(131)
@@ -166,7 +249,7 @@ plt.tight_layout()
 plt.show()
 
 
-# ---
+# %%
 
 mock.sel(kind=["fg", "sum", "delta"]).plot(
     col="kind", x="times", norm=simutils.symlognorm()
