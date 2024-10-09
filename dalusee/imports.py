e7c7568f5a9341cc644f118c48babb6ff59b713c
Rugved Pund <rugvedpund@gmail.com>
2024-10-09 15:55:36 -0400

    reorganize file structure

---
+++ dalusee/imports.py
@@ -0,0 +1,20 @@
+import argparse
+import pickle
+import sys
+
+
+import lusee
+import numpy as np
+import xarray as xr
+import xarray_einstats
+import jax
+import optax
+import jax.numpy as jnp
+# WARN: stupid jax doesn't like float64 by default
+jax.config.update("jax_enable_x64", True)
+from typing import Dict, List, Tuple
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from tqdm import tqdm
